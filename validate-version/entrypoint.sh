cc0a90c2133c59aaae91ba8581e0ed019b33e604
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-01 14:19:22 +0100

    ls root

--- validate-version/entrypoint.sh
+++ validate-version/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 git fetch origin master
 
-eval "ls"
-eval "find / validate.py"
-python validate.py "$(git show origin/master:package.json)"
+eval "ls /"
+python /validate.py "$(git show origin/master:package.json)"
