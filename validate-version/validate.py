01eaa130f9d3a0f12af3b786e62bbc7aeb74ad05
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-01 12:08:57 +0100

    python 3 bro

--- validate-version/validate.py
+++ validate-version/validate.py
@@ -8,8 +8,8 @@ master_version = json.loads(master_package_json)['version']
 new_version = json.load(open('package.json'))['version']
 
 if parse_version(new_version) <= parse_version(master_version):
-  print 'Version {} must be greater than current version {}'.format(new_version, master_version)
+  print('Version {} must be greater than current version {}'.format(new_version, master_version))
   exit(1)
 else:
-  print 'All good. Version {} is greater than current version {}'.format(new_version, master_version)
+  print('All good. Version {} is greater than current version {}'.format(new_version, master_version))
   exit(0)
