e9a32d17b78a160c02ad23b9926cf6db9a45a9cc
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 13:57:26 +0100

    is prod or not

--- deploy/library/entrypoint.sh
+++ deploy/library/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 git clone https://github.com/investoo/actions.git actions-repository
+echo $1
 
-./actions-repository/utils/gcloud_login.sh
+./actions-repository/utils/gcloud_login.sh $1
