a1730dad322b06e2cbaff7b9be7514bc2c7f1e0f
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 19:14:21 +0100

    goddamn unterminated quote string

--- utils/gcloud_upload_storage.sh
+++ utils/gcloud_upload_storage.sh
@@ -10,7 +10,7 @@ $TARGET_VERSION
 EOF
 
 echo $TARGET_VERSION
-echo "$MAJOR_VERSION
+echo $MAJOR_VERSION
 
 echo "gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION}/"
 # gsutil -m cp -r -z dist/* gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION}/
