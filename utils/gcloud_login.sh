2064ee4c6faa54eaddcfc913bf22d2153bfe53d1
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 17:56:46 +0100

    remove jq, I don't need you

--- utils/gcloud_login.sh
+++ utils/gcloud_login.sh
@@ -3,9 +3,8 @@
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-echo "${GCLOUD_SERVICE_KEY}" | base64 -d > keyfile.json
-ls
+echo "${GCLOUD_SERVICE_KEY}" | base64 -d > "${HOME}/gcloud.json" 
 
-gcloud auth activate-service-account --key-file=keyfile.json
+gcloud auth activate-service-account --key-file="${HOME}/gcloud.json" 
 # gcloud config set project ${GCLOUD_PROJECT_ID}
 # gcloud config set compute/zone ${GCLOUD_ZONE}
