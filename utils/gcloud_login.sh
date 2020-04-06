02c0dabc1cd1062f019956cba79b1b150387ba38
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 16:29:45 +0100

    remove setting vars

--- utils/gcloud_login.sh
+++ utils/gcloud_login.sh
@@ -3,9 +3,6 @@
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-export GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY
-export GOOGLE_PROJECT_ID=$GCLOUD_PROJECT_ID
-
 gcloud auth activate-service-account --key-file=${GCLOUD_SERVICE_KEY}
-gcloud config set project ${GOOGLE_PROJECT_ID}
+gcloud config set project ${GCLOUD_PROJECT_ID}
 gcloud config set compute/zone ${GCLOUD_ZONE}
