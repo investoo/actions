415139ee366000cd5b468501822f7361e1f2e305
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 18:09:52 +0100

    force fail temp

--- utils/gcloud_upload_storage.sh
+++ utils/gcloud_upload_storage.sh
@@ -8,3 +8,4 @@ TARGET_VERSION=$(python -c "import json; print json.load(open('package.json'))['
 MAJOR_VERSION_FOLDER="${BASH_REMATCH[1]}"
 echo "gs://${GCLOUD_BUCKET_NAME}/${GH_PROJECT_NAME}/"
 # gsutil -m cp -r -z dist/* gs://${BUCKET_NAME}/${PROJECT_NAME}/
+exit 0
