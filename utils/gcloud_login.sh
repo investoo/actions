afec59ff49b5e71a07e8ce51e8456bf2be4ead9c
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 16:54:07 +0100

    show me the money

--- utils/gcloud_login.sh
+++ utils/gcloud_login.sh
@@ -3,12 +3,10 @@
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-echo $GCLOUD_SERVICE_KEY > keyfile.json
-
-ls
+$GCLOUD_SERVICE_KEY < keyfile.json
 
 cat keyfile.json
 
-gcloud auth activate-service-account --key-file=keyfile.json
-gcloud config set project ${GCLOUD_PROJECT_ID}
-gcloud config set compute/zone ${GCLOUD_ZONE}
+# gcloud auth activate-service-account --key-file=keyfile.json
+# gcloud config set project ${GCLOUD_PROJECT_ID}
+# gcloud config set compute/zone ${GCLOUD_ZONE}
