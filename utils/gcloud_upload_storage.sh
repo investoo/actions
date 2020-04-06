e2455ae0b4b656a6d32b7065f9d5930e8b29bebb
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 19:11:14 +0100

    POSIX baby

--- utils/gcloud_upload_storage.sh
+++ utils/gcloud_upload_storage.sh
@@ -4,11 +4,13 @@ set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
 TARGET_VERSION=$(python -c "import json; print json.load(open('package.json'))['version']")
-IFS='.'
-read -a VERSION_SPLIT <<< "$TARGET_VERSION"
+
+IFS='.' read -r MAJOR_VERSION string <<EOF
+$TARGET_VERSION
+EOF
 
 echo $TARGET_VERSION
-echo $VERSION_SPLIT[0]
+echo "$MAJOR_VERSION
 
 echo "gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION}/"
 # gsutil -m cp -r -z dist/* gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION}/
