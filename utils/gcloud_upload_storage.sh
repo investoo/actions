4087e65eb906b110da8186b549dfa8c4c7525add
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 18:15:13 +0100

    show me gs route

--- utils/gcloud_upload_storage.sh
+++ utils/gcloud_upload_storage.sh
@@ -5,7 +5,9 @@ unset CLOUDSDK_CORE_PROJECT
 
 TARGET_VERSION=$(python -c "import json; print json.load(open('package.json'))['version']")
 [[ $TARGET_VERSION =~ ([0-9]+)\.[0-9]+ ]]
+
 MAJOR_VERSION_FOLDER="${BASH_REMATCH[1]}"
-echo "gs://${GCLOUD_BUCKET_NAME}/${GH_PROJECT_NAME}/"
-# gsutil -m cp -r -z dist/* gs://${BUCKET_NAME}/${PROJECT_NAME}/
+
+echo "gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION_FOLDER}/"
+# gsutil -m cp -r -z dist/* gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION_FOLDER}/
 exit 0
