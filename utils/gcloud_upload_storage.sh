3719af08ad5f16c95a120294a8d62fb6cf2f4435
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 18:23:02 +0100

    check target version

--- utils/gcloud_upload_storage.sh
+++ utils/gcloud_upload_storage.sh
@@ -4,8 +4,9 @@ set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
 TARGET_VERSION=$(python -c "import json; print json.load(open('package.json'))['version']")
-[[ $TARGET_VERSION =~ ([0-9]+)\.[0-9]+ ]]
+# [[ $TARGET_VERSION =~ ([0-9]+)\.[0-9]+ ]]
 
+echo $TARGET_VERSION
 MAJOR_VERSION_FOLDER="${BASH_REMATCH[1]}"
 
 echo "gs://${BUCKET_NAME}/${CI_PROJECT_NAME}/v${MAJOR_VERSION_FOLDER}/"
