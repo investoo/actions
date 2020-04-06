db5c67c24a28ac47cf1afad0018c60bc69b78447
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-06 18:01:53 +0100

    next steps

--- deploy/library/entrypoint.sh
+++ deploy/library/entrypoint.sh
@@ -3,3 +3,7 @@
 git clone https://github.com/investoo/actions.git actions-repository
 
 ./actions-repository/utils/gcloud_login.sh
+
+./actions-repository/utils/npm_build.sh
+
+./actions-repository/utils/gcloud_upload_storage.sh
