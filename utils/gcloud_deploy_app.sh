ef6e1e0cdcc0a7e3c06e9921edb46ce41fd0e18d
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-07 14:20:01 +0100

    setup service deployment scripts

---
+++ utils/gcloud_deploy_app.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+echo "Deploying app to App Engine"
+
+set -x -o nounset -o errexit
+unset CLOUDSDK_CORE_PROJECT
+
+touch .npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > .npmrc
+
+# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < app.yml > app_generated.yml
+ls
+cat app_generated.yml
+cat .npmrc
+
+# gcloud app deploy app_generated.yml
+# gcloud app deploy dispatch.yml
--- utils/npm_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-echo "Installing dependencies and building files"
-
-npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN_READ}
-npm ci
-npm run build
