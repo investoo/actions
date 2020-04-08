b4dfa173ebd4f140cc6d8feab9306ac13da3ace8
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 13:37:23 +0100

    view workspace

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -8,7 +8,7 @@ unset CLOUDSDK_CORE_PROJECT
 # touch ${HOME}/.npmrc
 # echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > ${HOME}/.npmrc
 
-ls /github
+ls /github/workspace
 
 # envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 
