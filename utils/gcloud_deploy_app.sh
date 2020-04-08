891ce9b299c2c181dfc21e22897c6ab46b985dbc
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 11:36:00 +0100

    deploy complete

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -8,12 +8,11 @@ unset CLOUDSDK_CORE_PROJECT
 touch .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > .npmrc
 
-# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
 
-ls
+
+gcloud app deploy app_generated.yml
+# gcloud app deploy dispatch.yml
 
 exit 1
-
-# gcloud app deploy app_generated.yml
-# gcloud app deploy dispatch.yml
