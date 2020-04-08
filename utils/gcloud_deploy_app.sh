4763317b0f640412d8ecad307c25c9792120c06f
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 11:25:07 +0100

    try ls this directory

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -9,7 +9,7 @@ touch .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > .npmrc
 
 # envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
-ls
+ls ~/
 
 exit 1
 
