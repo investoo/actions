db0c656606a57347e93a63284b1e6610d7e4558c
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 13:55:39 +0100

    show hidden files

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -7,7 +7,7 @@ unset CLOUDSDK_CORE_PROJECT
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > .npmrc
 
-ls
+ls -la
 cat .npmrc
 
 # envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
