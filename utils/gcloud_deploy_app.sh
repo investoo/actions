812b4193b4c0f05235a5b3644dc7dc2ac279c93d
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 11:04:47 +0100

    now try copying app.yml into root

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -8,12 +8,10 @@ unset CLOUDSDK_CORE_PROJECT
 touch .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > .npmrc
 
-echo $DEPLOY_ENV
-cat "deploy/${DEPLOY_ENV}/app.yml"
-# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < app.yml > app_generated.yml
-# ls
-# cat app_generated.yml
-# cat .npmrc
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < app.yml > app_generated.yml
+ls
+cat app_generated.yml
+cat .npmrc
 
 # gcloud app deploy app_generated.yml
 # gcloud app deploy dispatch.yml
