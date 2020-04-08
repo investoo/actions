95196dfc4bb170ce3f15741bf18077814aff8e33
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 12:18:08 +0100

    npmrc please

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -5,13 +5,13 @@ echo "Deploying app to App Engine"
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-touch .npmrc
+touch ~/.npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > ~/.npmrc
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
 
-ls
+ls ~/
 
 gcloud app deploy app_generated.yml
 # gcloud app deploy dispatch.yml
