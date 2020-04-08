23538be8eb61c9710fd47eccf2e65f0c90faa006
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 13:02:44 +0100

    npmrc in home directory?

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -5,17 +5,10 @@ echo "Deploying app to App Engine"
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-ls ${HOME}
-
-touch ../../.npmrc
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > ../../.npmrc
+touch ${HOME}/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > ${HOME}/.npmrc
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
-
-ls ../
-echo "hi"
-ls ../../
-# cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
 
 gcloud app deploy app_generated.yml
 # gcloud app deploy dispatch.yml
