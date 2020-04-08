f717348033e53e9dc7cd5cfb5236f27a2d1b9467
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 16:53:09 +0100

    fix test action

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -2,14 +2,12 @@
 
 NEEDS_DOCKER=$1
 
-# Set up NPM packages
 npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN_READ
 npm ci
 
-if [$NEEDS_DOCKER]
-then
+if [[ -z "${NEEDS_DOCKER}" ]]; then
+  npm run test
+else
   docker-compose up -d
   docker-compose run --rm $GH_PROJECT_NAME npm run test
-else
-  npm run test
 fi
