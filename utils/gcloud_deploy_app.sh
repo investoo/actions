f9fa71fe0404da87a86b52169b3fb05f49c3431e
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 12:26:08 +0100

    new approach

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -5,15 +5,14 @@ echo "Deploying app to App Engine"
 set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
-touch ~/.npmrc
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN_READ}" > ~/.npmrc
+ls
 
-envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
-cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
+# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
+# cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
 
-ls ~/
+# ls ~/
 
-gcloud app deploy app_generated.yml
+# gcloud app deploy app_generated.yml
 # gcloud app deploy dispatch.yml
 
 exit 1
