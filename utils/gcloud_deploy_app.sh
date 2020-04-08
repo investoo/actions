1d96a37cf4426b8e66196f16bc41d19b4c21d7fc
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-08 12:41:07 +0100

    can't find deploy directory

--- utils/gcloud_deploy_app.sh
+++ utils/gcloud_deploy_app.sh
@@ -10,7 +10,9 @@ ls ${HOME}
 npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN_READ}
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
-cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
+
+ls
+# cp "../../dispatch/${DEPLOY_ENV}.yml" dispatch.yml
 
 gcloud app deploy app_generated.yml
 # gcloud app deploy dispatch.yml
