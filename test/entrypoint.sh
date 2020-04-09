b58b79612608bf7ce744f490bacdaf076142db61
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 19:24:44 +0100

    add docker into the mix

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,8 +6,6 @@ npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm ci
 
 if [ $NEEDS_DOCKER == true ]; then
-  curl -L --fail https://github.com/docker/compose/releases/download/1.24.0/run.sh -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
   docker-compose up -d
   docker-compose run --rm $GH_PROJECT_NAME npm run test
 else
