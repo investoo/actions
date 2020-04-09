1c71fb977685e1b223fc3ef3e244bf41242b9b44
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 18:06:25 +0100

    been here before

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -8,7 +8,7 @@ echo $NEEDS_DOCKER
 # npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 # npm ci
 
-if [$NEEDS_DOCKER == true]; then
+if [$NEEDS_DOCKER = true]; then
   echo "docker!"
   # docker-compose up -d
   # docker-compose run --rm $GH_PROJECT_NAME npm run test
