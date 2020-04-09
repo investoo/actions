363c2abebbbaf2e8c68117cfab667b8de7d88f0b
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 12:24:34 +0100

    node8 not 10

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -3,4 +3,4 @@
 set -x -o nounset -o errexit
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=handle --runtime=nodejs10 --region=${REGION} --memory=${MEMORY} --trigger-http
+gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=handle --runtime=nodejs8 --region=${REGION} --memory=${MEMORY} --trigger-http
