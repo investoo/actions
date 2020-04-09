65d44ae31c2ea604668a241af6bd2acce40a5e37
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 14:32:39 +0100

    root again?

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -7,7 +7,7 @@ unset CLOUDSDK_CORE_PROJECT
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
-ls ${HOME}
+ls ../../
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 
