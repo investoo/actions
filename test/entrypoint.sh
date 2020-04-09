5e0ad6a01a282569f5b38e5fccfa5bd74ca8cbbf
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 17:43:40 +0100

    string baby

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -5,14 +5,14 @@ NEEDS_DOCKER=$1
 echo $1 
 echo $NEEDS_DOCKER
 
-npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
-npm ci
+# npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
+# npm ci
 
-if [$NEEDS_DOCKER == true]; then
+if [$NEEDS_DOCKER == "true"]; then
   echo "docker!"
-  docker-compose up -d
-  docker-compose run --rm $GH_PROJECT_NAME npm run test
+  # docker-compose up -d
+  # docker-compose run --rm $GH_PROJECT_NAME npm run test
 else
   echo "no docker!"
-  npm run test
+  # npm run test
 fi
