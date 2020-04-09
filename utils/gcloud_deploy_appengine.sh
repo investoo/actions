3c9649d31e85579be0047502f2123d96592298be
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 16:07:09 +0100

    oopds, try deploy this

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -6,9 +6,13 @@ set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-echo "../../app/dispatch/${DEPLOY_ENV}.yml" > dispatch.yml
+echo ../../app/dispatch/${DEPLOY_ENV}.yml > dispatch.yml
 
-envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
+echo ../../app/dispatch/${DEPLOY_ENV}.yml
+echo "nice"
+cat dispatch.yml
 
-gcloud app deploy app_generated.yml
-gcloud app deploy dispatch.yml
+# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
+
+# gcloud app deploy app_generated.yml
+# gcloud app deploy dispatch.yml
