3ef52de822b47747cd101131a05eb5b24aa21834
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 14:41:15 +0100

    deploy dispatch.yml

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -6,12 +6,11 @@ set -x -o nounset -o errexit
 unset CLOUDSDK_CORE_PROJECT
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-
-ls ../../app
+echo "../../app/dispatch/${DEPLOY_ENV}.yml" > dispatch.yml
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 
 # gcloud app deploy app_generated.yml
-# gcloud app deploy dispatch.yml
+gcloud app deploy dispatch.yml
 
 exit 1
