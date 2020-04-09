62260996a30ca3ff792cd9513d8de8db5e196b40
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 11:45:03 +0100

    export login stuff as vars

--- utils/gcloud_login.sh
+++ utils/gcloud_login.sh
@@ -7,6 +7,9 @@ unset CLOUDSDK_CORE_PROJECT
 
 echo "${GCLOUD_SERVICE_KEY}" | base64 -d > "${HOME}/gcloud.json" 
 
+export GCLOUD_SERVICE_KEY="${HOME}/gcloud.json" 
+export GOOGLE_PROJECT_ID=$GCLOUD_PROJECT_ID
+
 gcloud auth activate-service-account --key-file="${HOME}/gcloud.json" 
 gcloud config set project ${GCLOUD_PROJECT_ID}
 gcloud config set compute/zone ${GCLOUD_ZONE}
