fdbfaef3792d83fdf9fdba2984e75cf434b36184
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 13:23:24 +0100

    publish library during deployment

--- deploy/library/entrypoint.sh
+++ deploy/library/entrypoint.sh
@@ -6,4 +6,6 @@ git clone https://github.com/investoo/actions.git actions-repository
 
 ./actions-repository/utils/npm_build.sh
 
+./actions-repository/utils/npm_publish.sh
+
 ./actions-repository/utils/gcloud_upload_storage.sh
