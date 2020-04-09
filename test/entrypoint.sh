d6d750cca64f77a36c73f686d25af82eb8e675bc
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 17:01:21 +0100

    test npmrc exists

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,6 +6,8 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm ci
 
+cat .npmrc
+
 if [$NEEDS_DOCKER]; then
   docker-compose up -d
   docker-compose run --rm $GH_PROJECT_NAME npm run test
