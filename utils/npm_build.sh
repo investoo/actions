78826846f0de038be52f49af3c7364d5c2ea56bb
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 09:47:14 +0100

    try without if statement

--- utils/npm_build.sh
+++ utils/npm_build.sh
@@ -3,13 +3,4 @@
 npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
 npm ci
 
-echo $BUILD_MODE
-
-
-if [$BUILD_MODE != 'production']; then
-  echo $BUILD_MODE
-#   npm run build -- --mode ${BUILD_MODE}
-else
- echo "no build mode"
-#   npm run build
-fi
+npm run build -- --mode ${BUILD_MODE}
