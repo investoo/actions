9f4ae0bb7389260d42ff5b7552f7b8668870cf29
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-09 16:42:51 +0100

    deploy GAE works 🎉

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -9,7 +9,7 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 cp ../../app/dispatch/${DEPLOY_ENV}.yml dispatch.yml
 
 
-# envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 
-# gcloud app deploy app_generated.yml
+gcloud app deploy app_generated.yml
 gcloud app deploy dispatch.yml
