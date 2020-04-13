19d275a0afe0fd3d7bf350c78174f2c9b145dca6
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-13 17:57:25 +0100

    fingers crossed now

--- notify-slack/entrypoint.sh
+++ notify-slack/entrypoint.sh
@@ -1,22 +1,20 @@
 #!/bin/sh -l
 
-ls /
+DEPLOYMENT_TARGET=$1
+DEPLOYMENT_SUCCESSFUL=$2
+GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY
+GITHUB_WORKFLOW_URL=$GITHUB_REPO_URL/actions/runs/$GITHUB_RUN_ID
+GITHUB_COMMIT_URL=$GITHUB_REPO_URL/commit/$GITHUB_SHA
 
-# DEPLOYMENT_TARGET=$1
-# DEPLOYMENT_SUCCESSFUL=$2
-# GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY
-# GITHUB_WORKFLOW_URL=$GITHUB_REPO_URL/actions/runs/$GITHUB_RUN_ID
-# GITHUB_COMMIT_URL=$GITHUB_REPO_URL/commit/$GITHUB_SHA
-
-# if [ $DEPLOYMENT_SUCCESSFUL = Success ]; then
-#   DEPLOYMENT_MESSAGE=":white_check_mark: *Deployment Successful*"
-# else
-#   DEPLOYMENT_MESSAGE=":x: *Deployment Failed*"
-# fi
+if [ $DEPLOYMENT_SUCCESSFUL = Success ]; then
+  DEPLOYMENT_MESSAGE=":white_check_mark: *Deployment Successful*"
+else
+  DEPLOYMENT_MESSAGE=":x: *Deployment Failed*"
+fi
 
 
-# envsubst < template.json > message.json
+envsubst < /template.json > message.json
 
-# cat message.json
+cat message.json
 
 exit 1
