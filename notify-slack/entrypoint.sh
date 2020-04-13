1ff45dbfb0281a4f5278b5771394a6fb8bd4f0c4
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-13 17:50:01 +0100

    update dockerfile

--- notify-slack/entrypoint.sh
+++ notify-slack/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-ls
+ls -la
 
 DEPLOYMENT_TARGET=$1
 DEPLOYMENT_SUCCESSFUL=$2
@@ -8,14 +8,12 @@ GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY
 GITHUB_WORKFLOW_URL=$GITHUB_REPO_URL/actions/runs/$GITHUB_RUN_ID
 GITHUB_COMMIT_URL=$GITHUB_REPO_URL/commit/$GITHUB_SHA
 
-if [ $DEPLOYMENT_SUCCESSFUL = true ]; then
+if [ $DEPLOYMENT_SUCCESSFUL = Success ]; then
   DEPLOYMENT_MESSAGE=":white_check_mark: *Deployment Successful*"
 else
   DEPLOYMENT_MESSAGE=":x: *Deployment Failed*"
 fi
 
-echo $DEPLOYMENT_SUCCESSFUL
-echo $DEPLOYMENT_TARGET
 
 envsubst < template.json > message.json
 
