497f518e3a142c0e4b46b5c2c0ec861af5b5caf9
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-13 17:02:34 +0100

    set up deployment message

---
+++ notify-slack/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+DEPLOYMENT_TARGET=$1
+DEPLOYMENT_SUCCESSFUL=$2
+GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY
+GITHUB_WORKFLOW_URL=$GITHUB_REPO_URL/actions/runs/$GITHUB_RUN_ID
+GITHUB_COMMIT_URL=$GITHUB_REPO_URL/commit/$GITHUB_SHA
+
+if [ $DEPLOYMENT_SUCCESSFUL = true ]; then
+  DEPLOYMENT_MESSAGE=":white_check_mark: *Deployment Successful*"
+else
+  DEPLOYMENT_MESSAGE=":x: *Deployment Failed*"
+fi
+
+envsubst < template.json > message.json
+
+cat message.json
+
+exit 1
