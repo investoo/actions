188359b75a804930a210c6363e7539d109c774c1
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-15 10:10:58 +0100

    install via github package registry

--- utils/npm_install.sh
+++ utils/npm_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-
 npm config set //npm.pkg.github.com/:_authToken ${NPM_TOKEN}
+echo "registry=https://npm.pkg.github.com/investoo" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}" > .npmrc
 npm ci
