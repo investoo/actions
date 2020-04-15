0fe3c9f6842ea2eaf7052157d98bcbb799c315ba
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-15 17:12:23 +0100

    remove ls

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -7,7 +7,6 @@ git clone https://github.com/investoo/actions.git actions-repository
 ./actions-repository/utils/npm_install.sh
 
 if [ $NEEDS_DOCKER = true ]; then
-  ls
   docker-compose run --rm $GH_PROJECT_NAME npm run test
 else
   npm run test
