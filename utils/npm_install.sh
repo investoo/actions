057a824555babc9416a9035aad11ba52ce07a5b0
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-15 10:23:44 +0100

    how about now!?!

--- utils/npm_install.sh
+++ utils/npm_install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 npm config set //npm.pkg.github.com/:_authToken ${NPM_TOKEN}
-echo "registry=https://npm.pkg.github.com/investoo" > .npmrc
-echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}" > .npmrc
+echo "
+registry=https://npm.pkg.github.com/investoo
+//npm.pkg.github.com/:_authToken=${NPM_TOKEN}
+" > .npmrc
 
 cat .npmrc
 ls
