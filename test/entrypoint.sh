a5a71184deaa45f75b37c8950652b0601fdb8eac
Louis <louis.dalbe@investoogroup.com>
2020-04-20 17:04:20 +0100

    test some lines

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -7,6 +7,8 @@ git clone https://github.com/investoo/actions.git actions-repository
 ./actions-repository/utils/npm_install.sh
 
 if [ $NEEDS_DOCKER = true ]; then
+  pwd
+  ls ./
   docker-compose run --workdir /github/workspace --rm $GH_PROJECT_NAME npm run test
 else
   npm run test
