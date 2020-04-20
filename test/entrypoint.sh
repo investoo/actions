869737d9dcfdecf530695427efd0a88db6dedce5
Louis <louis.dalbe@investoogroup.com>
2020-04-20 16:45:59 +0100

    check if changing workspace helps

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -7,7 +7,7 @@ git clone https://github.com/investoo/actions.git actions-repository
 ./actions-repository/utils/npm_install.sh
 
 if [ $NEEDS_DOCKER = true ]; then
-  docker-compose run --rm $GH_PROJECT_NAME npm run test
+  docker-compose run --workdir /github/workspace --rm $GH_PROJECT_NAME npm run test
 else
   npm run test
-fi    
+fi 
