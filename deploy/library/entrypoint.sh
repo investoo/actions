36d864d6c11d5b4ae2d44b1d5f5e9f1b30a2baf4
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-24 17:56:11 +0100

    update library deploy

--- deploy/library/entrypoint.sh
+++ deploy/library/entrypoint.sh
@@ -10,13 +10,13 @@ git clone https://github.com/investoo/actions.git actions-repository
 
 ./actions-repository/utils/npm_build.sh
 
-./actions-repository/utils/npm_create_npmrc.sh
 
 ./actions-repository/utils/gcloud_upload_storage.sh
 
 if [ $PUBLISH_PACKAGE = true ]; then
+  ./actions-repository/utils/npm_create_npmrc.sh
   echo "Publishing library"
   ./actions-repository/utils/npm_publish.sh
 else
-  echo "Library not published"
+  echo "Not publishing library"
 fi
