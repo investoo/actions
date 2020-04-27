58cc331f9ff2636b344264245080aba27f390849
Louis <louis.dalbe@investoogroup.com>
2020-04-27 16:57:25 +0100

    cat the app_generated again

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -10,6 +10,8 @@ cp ../../app/dispatch/${DEPLOY_ENV}.yml dispatch.yml
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.yml" > app_generated.yml
 
+echo "Using app_generated.yml"
+cat app_generated.yml
 
 echo "Using gcloud config: "
 gcloud config list --all
