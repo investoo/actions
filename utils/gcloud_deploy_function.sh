b5e1364d2440842118a2bee0b6572c5a473e2817
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-28 02:56:23 +0100

    to new file

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -5,14 +5,14 @@ echo "Deploying app to Cloud Functions"
 set -x -o nounset -o errexit
 
 # test -f .env.yaml || touch .env.yaml
-envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env.yaml
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
 
-cat .env.yaml
+cat .env_generated.yaml
 
 if [ $TRIGGER = HTTP ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http --env-vars-file .env.yaml
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http --env-vars-file .env_generated.yaml
 elif [ $TRIGGER = TOPIC ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-topic=${TRIGGER_TOPIC} --env-vars-file .env.yaml
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-topic=${TRIGGER_TOPIC} --env-vars-file .env_generated.yaml
 else
   echo "Trigger ${TRIGGER} not found"
   exit 1
