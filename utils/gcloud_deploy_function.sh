9eb02ce16429f70228b9966609a4379682ec8ba5
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-28 03:00:11 +0100

    generate env file for cloud functions

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -4,10 +4,8 @@ echo "Deploying app to Cloud Functions"
 
 set -x -o nounset -o errexit
 
-# test -f .env.yaml || touch .env.yaml
+test -f .env.yaml || touch .env.yaml
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
-
-cat .env_generated.yaml
 
 if [ $TRIGGER = HTTP ]; then
   gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http --env-vars-file .env_generated.yaml
