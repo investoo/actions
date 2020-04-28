48484feb40a5b5f83167176182262dcccda0945d
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-28 02:46:01 +0100

    show contents of env yaml

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -7,6 +7,8 @@ set -x -o nounset -o errexit
 test -f .env.yaml || touch .env.yaml
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < ".env.yaml" > .env.yaml
 
+cat .env.yaml
+
 if [ $TRIGGER = HTTP ]; then
   gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http --env-vars-file .env.yaml
 elif [ $TRIGGER = TOPIC ]; then
