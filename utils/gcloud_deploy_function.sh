10b5df18891a737e49633c552d717cde7f327a3b
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-04-28 02:38:39 +0100

    set .env vars for cloud functions

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -4,10 +4,12 @@ echo "Deploying app to Cloud Functions"
 
 set -x -o nounset -o errexit
 
+envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < ".env.yml" > .env.yml
+
 if [ $TRIGGER = HTTP ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-http --env-vars-file .env.yaml
 elif [ $TRIGGER = TOPIC ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-topic=${TRIGGER_TOPIC}
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --trigger-topic=${TRIGGER_TOPIC} --env-vars-file .env.yaml
 else
   echo "Trigger ${TRIGGER} not found"
   exit 1
