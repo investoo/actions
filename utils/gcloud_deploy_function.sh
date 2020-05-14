35d9c51201807c04bbdcce688669d4fed9b79669
Louis <louis.dalbe@investoogroup.com>
2020-05-14 14:21:35 +0100

    dont default timeout

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -8,9 +8,9 @@ test -f .env.yaml || touch .env.yaml
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
 
 if [ $TRIGGER = HTTP ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --timeout=${TIMEOUT:-120} --trigger-http --env-vars-file=.env_generated.yaml
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --timeout=${TIMEOUT} --trigger-http --env-vars-file=.env_generated.yaml
 elif [ $TRIGGER = TOPIC ]; then
-  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --timeout=${TIMEOUT:-120} --trigger-topic=${TRIGGER_TOPIC} --env-vars-file=.env_generated.yaml
+  gcloud functions deploy ${GH_PROJECT_NAME} --entry-point=${ENTRY_POINT} --runtime=${RUNTIME} --region=${REGION} --memory=${MEMORY} --timeout=${TIMEOUT} --trigger-topic=${TRIGGER_TOPIC} --env-vars-file=.env_generated.yaml
 else
   echo "Trigger ${TRIGGER} not found"
   exit 1
