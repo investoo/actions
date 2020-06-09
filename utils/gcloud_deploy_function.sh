01fa9e449880a6f78ef3b3c727509af1e7380f33
Louis <louis.dalbe@investoogroup.com>
2020-06-09 15:29:21 +0100

    Add allow-unauthenticated flag to deploy function action

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -8,7 +8,7 @@ test -f .env.yaml || touch .env.yaml
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
 
 if [ "$TRIGGER" = HTTP ]; then
-  gcloud functions deploy "${GH_PROJECT_NAME}" --entry-point="${ENTRY_POINT}" --runtime="${RUNTIME}" --region="${REGION}" --memory="${MEMORY}" --timeout="${TIMEOUT}" --trigger-http --env-vars-file=.env_generated.yaml
+  gcloud functions deploy "${GH_PROJECT_NAME}" --entry-point="${ENTRY_POINT}" --runtime="${RUNTIME}" --region="${REGION}" --memory="${MEMORY}" --timeout="${TIMEOUT}" --trigger-http --allow-unauthenticated --env-vars-file=.env_generated.yaml
 elif [ "$TRIGGER" = TOPIC ]; then
   gcloud functions deploy "${GH_PROJECT_NAME}" --entry-point="${ENTRY_POINT}" --runtime="${RUNTIME}" --region="${REGION}" --memory="${MEMORY}" --timeout="${TIMEOUT}" --trigger-topic="${TRIGGER_TOPIC}" --env-vars-file=.env_generated.yaml
 else
