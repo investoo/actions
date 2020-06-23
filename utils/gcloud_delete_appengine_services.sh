0540310c591ac64be216e1a717fc53e535ab899a
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-06-23 10:46:08 +0100

    delete old app services if more than 2 exist

---
+++ utils/gcloud_delete_appengine_services.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+GCLOUD_PROJECT_ID=tech-microservices-staging
+GH_PROJECT_NAME=partners
+echo "Checking for stopped App Engine services..."
+
+STOPPED_SERVICES=$(gcloud app  --project ${GCLOUD_PROJECT_ID} versions list --filter="SERVICE = ${GH_PROJECT_NAME} AND SERVING_STATUS = STOPPED" --sort-by="LAST_DEPLOYED" --format="value(VERSION.ID)" &)
+STOPPED_SERVICES_COUNT=$(echo "$STOPPED_SERVICES" | wc -l)
+
+if [ "$STOPPED_SERVICES_COUNT" -gt 2 ]; then
+  NUMBER_OF_SERVICES_TO_KILL=$((STOPPED_SERVICES_COUNT - 2))
+  SERVICES_TO_KILL=$(echo "$STOPPED_SERVICES" | head -n $NUMBER_OF_SERVICES_TO_KILL)
+  echo "Deleting ${NUMBER_OF_SERVICES_TO_KILL} excess services..."
+  gcloud --project ${GCLOUD_PROJECT_ID} app versions delete $SERVICES_TO_KILL
+fi
