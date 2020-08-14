a74f7d5d01c8185a51459c8c59813b0d7e784e7a
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-08-14 17:04:44 +0100

    fix npmrc

--- utils/npm_create_npmrc.sh
+++ utils/npm_create_npmrc.sh
@@ -3,6 +3,6 @@
 echo "Creating .npmrc"
 
 echo "
-@investoo:registry=https://npm.pkg.github.com/investoo
+registry=https://npm.pkg.github.com/investoo
 //npm.pkg.github.com/:_authToken=${NPM_TOKEN}
 " > .npmrc
