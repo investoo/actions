aea256d3a9112e2099d95bba15051f3778de0353
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-08-14 16:28:49 +0100

    test

--- deploy/service/entrypoint.sh
+++ deploy/service/entrypoint.sh
@@ -4,11 +4,11 @@ git clone https://github.com/investoo/actions.git actions-repository
 
 ./actions-repository/utils/gcloud_login.sh
 
-if [ $NPM_OLD = true ]; then
-  ./actions-repository/utils/npm_config_old.sh
-else
+# if [ $NPM_OLD = true ]; then
+#   ./actions-repository/utils/npm_config_old.sh
+# else
   ./actions-repository/utils/npm_config.sh
-fi
+# fi
 
 ./actions-repository/utils/npm_create_npmrc.sh
 
