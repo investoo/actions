073cf454f8d7e959819b029a83935c0e7a51fd35
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-11-17 16:50:48 +0000

    only create npmrc if using github instead of npm

--- deploy/service/entrypoint.sh
+++ deploy/service/entrypoint.sh
@@ -8,9 +8,9 @@ if [ $NPM_OLD = true ]; then
   ./actions-repository/utils/npm_config_old.sh
 else
   ./actions-repository/utils/npm_config.sh
+  ./actions-repository/utils/npm_create_npmrc.sh
 fi
 
-./actions-repository/utils/npm_create_npmrc.sh
 
 ./actions-repository/utils/gcloud_delete_appengine_services.sh
 
