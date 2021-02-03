bbd000a41f50a09fb59ffde1b78bd12fc8ec83e2
Beau August <10343831+BeauAgst@users.noreply.github.com>
2021-02-03 10:05:56 +0000

    test env vars for firebase deploys

--- utils/gcloud_deploy_firebase.sh
+++ utils/gcloud_deploy_firebase.sh
@@ -4,6 +4,12 @@ echo "Deploying app to Firebase"
 
 set -x -o nounset -o errexit
 
+if [ "$DEPLOY_ENV" = prod ]; then
+  envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env > .env
+else
+  envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.staging > .env.staging
+fi
+
 export GOOGLE_PROJECT_ID=$GCLOUD_PROJECT_ID
 
 npx firebase use --add "${GOOGLE_PROJECT_ID}" --token "$FIREBASE_TOKEN"
