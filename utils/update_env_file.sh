219f6d55a51c0f79daaa2301fc29d54842ce81e0
Beau August <10343831+BeauAgst@users.noreply.github.com>
2021-02-03 10:56:20 +0000

    test exports

--- utils/update_env_file.sh
+++ utils/update_env_file.sh
@@ -1,5 +1,11 @@
+echo "Updating .env file"
+
+export -p
+
 if [ "$DEPLOY_ENV" = prod ]; then
   envsubst < .env > .env.tmp && mv .env.tmp .env
+  cat .env
 else
   envsubst < .env.staging > .env.tmp && mv .env.tmp .env.staging
+  cat .env.staging
 fi
