a3433daa369849858c47c03590c9481db2d02c6c
Beau August <10343831+BeauAgst@users.noreply.github.com>
2021-02-26 13:11:15 +0000

    install before deploy

--- deploy/function/entrypoint.sh
+++ deploy/function/entrypoint.sh
@@ -8,4 +8,6 @@ git clone https://github.com/investoo/actions.git actions-repository
 
 ./actions-repository/utils/npm_create_npmrc.sh
 
+./actions-repository/utils/npm_install.sh
+
 ./actions-repository/utils/gcloud_deploy_function.sh
