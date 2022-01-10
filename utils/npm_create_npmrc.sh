ba13cfb0196e56b8a5a2ce20703176198d39fb34
Seth-Carter <57359603+Seth-Carter@users.noreply.github.com>
2022-01-10 16:02:19 +0000

    Update npm_create_npmrc.sh

--- utils/npm_create_npmrc.sh
+++ utils/npm_create_npmrc.sh
@@ -2,9 +2,7 @@
 
 echo "Creating .npmrc"
 
-# The 'registry=https://npm.pkg.github.com/investoo" line breaks deployment for the 'monitor-failed-registrations' cloud function
 echo "
-registry=https://npm.pkg.github.com/investoo
 //npm.pkg.github.com/:_authToken=${NPM_TOKEN}
 " > .npmrc
 
