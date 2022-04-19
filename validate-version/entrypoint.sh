8ccf5f091b87b48791eddd7df164ec29c369ceb7
Beau August <10343831+BeauAgst@users.noreply.github.com>
2022-04-19 09:45:41 +0100

    testing github permissions issue

--- validate-version/entrypoint.sh
+++ validate-version/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+git --version
+
+git config --global safe.directory "$GITHUB_WORKSPACE"
+
+whoami
+
 git fetch origin master
 
 python /validate.py "$(git show origin/master:package.json)"
