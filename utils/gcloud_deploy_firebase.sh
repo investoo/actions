b204a028b818c33e507216067768992e0cf45158
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-11-14 11:39:43 +0000

    update key

--- utils/gcloud_deploy_firebase.sh
+++ utils/gcloud_deploy_firebase.sh
@@ -8,9 +8,10 @@ set -o xtrace
 
 export GOOGLE_PROJECT_ID=$GCLOUD_PROJECT_ID
 
-echo "npx firebase use --add '${GOOGLE_PROJECT_ID}' --token '$FIREBASE_TOKEN'"
-npx firebase use --add "${GOOGLE_PROJECT_ID}" --token "$FIREBASE_TOKEN"
+echo "npx firebase use --add '${GOOGLE_PROJECT_ID}' --token '1//03waaXqZ1gJCGCgYIARAAGAMSNwF-L9IrU-Y6s6QD6ooBjc0Zj7AWKMUmPUbWmwwu4-m6kfAmK5AARAMPwrS-EvxcGPcOdJ4aIa8'"
+npx firebase use --add "${GOOGLE_PROJECT_ID}" --token "1//03waaXqZ1gJCGCgYIARAAGAMSNwF-L9IrU-Y6s6QD6ooBjc0Zj7AWKMUmPUbWmwwu4-m6kfAmK5AARAMPwrS-EvxcGPcOdJ4aIa8"
 echo "npx firebase target:apply hosting '${GH_PROJECT_NAME}' '${GH_PROJECT_NAME}-${DEPLOY_ENV}'"
 npx firebase target:apply hosting "${GH_PROJECT_NAME}" "${GH_PROJECT_NAME}-${DEPLOY_ENV}"
-echo "npx firebase deploy --token '$FIREBASE_TOKEN' --only hosting:'${GH_PROJECT_NAME}'"
-npx firebase deploy --token "$FIREBASE_TOKEN" --only hosting:"${GH_PROJECT_NAME}"
+echo "npx firebase deploy --token '1//03waaXqZ1gJCGCgYIARAAGAMSNwF-L9IrU-Y6s6QD6ooBjc0Zj7AWKMUmPUbWmwwu4-m6kfAmK5AARAMPwrS-EvxcGPcOdJ4aIa8' --only hosting:'${GH_PROJECT_NAME}'"
+npx firebase deploy --token "1//03waaXqZ1gJCGCgYIARAAGAMSNwF-L9IrU-Y6s6QD6ooBjc0Zj7AWKMUmPUbWmwwu4-m6kfAmK5AARAMPwrS-EvxcGPcOdJ4aIa8" --only hosting:"${GH_PROJECT_NAME}"
+
