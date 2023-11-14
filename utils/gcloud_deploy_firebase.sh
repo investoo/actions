6d4b74df332af5d654004727cbc930a17b59cdf0
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-11-14 10:58:46 +0000

    update

--- utils/gcloud_deploy_firebase.sh
+++ utils/gcloud_deploy_firebase.sh
@@ -8,6 +8,9 @@ set -o xtrace
 
 export GOOGLE_PROJECT_ID=$GCLOUD_PROJECT_ID
 
+echo "npx firebase use --add '${GOOGLE_PROJECT_ID}' --token '$FIREBASE_TOKEN'"
 npx firebase use --add "${GOOGLE_PROJECT_ID}" --token "$FIREBASE_TOKEN"
+echo "npx firebase target:apply hosting '${GH_PROJECT_NAME}' '${GH_PROJECT_NAME}-${DEPLOY_ENV}'"
 npx firebase target:apply hosting "${GH_PROJECT_NAME}" "${GH_PROJECT_NAME}-${DEPLOY_ENV}"
+echo "npx firebase deploy --token '$FIREBASE_TOKEN' --only hosting:'${GH_PROJECT_NAME}'"
 npx firebase deploy --token "$FIREBASE_TOKEN" --only hosting:"${GH_PROJECT_NAME}"
