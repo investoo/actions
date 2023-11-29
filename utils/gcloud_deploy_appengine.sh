3af0f4ad44e8a53048aa01f594d19b7c6751a0a7
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-11-29 17:09:13 +0000

    remove

--- utils/gcloud_deploy_appengine.sh
+++ utils/gcloud_deploy_appengine.sh
@@ -14,16 +14,16 @@ envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < "deploy/${DEPLOY_ENV}/app.ym
 echo "Using app_generated.yml"
 cat app_generated.yml
 
-echo "Using gcloud config: "
-gcloud config list --all
+# echo "Using gcloud config: "
+# gcloud config list --all
 
-echo "Running gcloud app deploy app_generated.yml"
-gcloud app deploy app_generated.yml --verbosity=debug
+# echo "Running gcloud app deploy app_generated.yml"
+# gcloud app deploy app_generated.yml --verbosity=debug
 
-echo "Running gcloud app deploy dispatch.yml"
-gcloud app deploy dispatch.yml
+# echo "Running gcloud app deploy dispatch.yml"
+# gcloud app deploy dispatch.yml
 
-if [ -e cron.yml ]; then
-  echo "Running gcloud app deploy cron.yml"
-  gcloud app deploy cron.yml
-fi
+# if [ -e cron.yml ]; then
+#   echo "Running gcloud app deploy cron.yml"
+#   gcloud app deploy cron.yml
+# fi
