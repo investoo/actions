c14736e47ccfc112fa4500f9655986f720a2d95d
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-11-30 12:15:45 +0000

    update

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -7,9 +7,9 @@ ls
 set -x -o nounset -o errexit
 
 test -f .env.yaml || touch .env.yaml 
-# echo "DUMMY_VARIABLE: 'DUMMY_VARIABLE'" >> .env.yaml
 
 envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
+cat .env_generated.yaml
 
 if [ "$TRIGGER" = HTTP ]; then
   gcloud functions deploy "${GH_PROJECT_NAME}" --entry-point="${ENTRY_POINT}" --runtime="${RUNTIME}" --region="${REGION}" --memory="${MEMORY}" --timeout="${TIMEOUT}" --trigger-http --env-vars-file=.env_generated.yaml
