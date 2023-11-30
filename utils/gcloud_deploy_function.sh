3eecbcd1e33348767ce001f491b1368ca357171e
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-11-30 13:52:17 +0000

    update

--- utils/gcloud_deploy_function.sh
+++ utils/gcloud_deploy_function.sh
@@ -11,6 +11,7 @@ test -f .env.yaml || touch .env.yaml
 # envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')" < .env.yaml > .env_generated.yaml
 envsubst < .env.yaml > .env_generated.yaml
 
+echo "contents of .env_generated.yaml"
 cat .env_generated.yaml
 
 if [ "$TRIGGER" = HTTP ]; then
