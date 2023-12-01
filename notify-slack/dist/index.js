82efcb8a8d6943c30cb8d56861978b9d4c26dd3e
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 07:19:52 +0000

    add commit message

--- notify-slack/dist/index.js
+++ notify-slack/dist/index.js
@@ -5,11 +5,22 @@
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const main = () => {
-  console.log('hey')
-}
+const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-main()
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+const main = async () => {
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
+};
+
+main();
+
 module.exports = __webpack_exports__;
 /******/ })()
 ;
--- notify-slack/entrypoint.js
+++ notify-slack/entrypoint.js
@@ -1,5 +1,15 @@
-const main = () => {
-  console.log('hey')
-}
+const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-main()
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+const main = async () => {
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
+};
+
+main();
