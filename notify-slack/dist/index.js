831a3a1910ddb36174ca2218b4e5b9f1da504663
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 07:32:03 +0000

    uncomment

--- notify-slack/dist/index.js
+++ notify-slack/dist/index.js
@@ -30794,18 +30794,18 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-// const octokit = github.getOctokit(GITHUB_TOKEN);
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-// const { context = {} } = github;
-// const { pull_request } = context.payload;
+const { context = {} } = github;
+const { pull_request } = context.payload;
 
 const main = async () => {
   console.log('The GITHUB_TOKEN value', GITHUB_TOKEN)
-  // await octokit.issues.createComment({
-  //   ...context.repo,
-  //   issue_number: pull_request.number,
-  //   body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
-  // });
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
 };
 
 main();
--- notify-slack/entrypoint.js
+++ notify-slack/entrypoint.js
@@ -2,18 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-// const octokit = github.getOctokit(GITHUB_TOKEN);
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-// const { context = {} } = github;
-// const { pull_request } = context.payload;
+const { context = {} } = github;
+const { pull_request } = context.payload;
 
 const main = async () => {
   console.log('The GITHUB_TOKEN value', GITHUB_TOKEN)
-  // await octokit.issues.createComment({
-  //   ...context.repo,
-  //   issue_number: pull_request.number,
-  //   body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
-  // });
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
 };
 
 main();
