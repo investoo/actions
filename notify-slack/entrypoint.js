1dddc6d443dca611d538d3322e0cbd67ab62ec9d
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 07:21:13 +0000

    comment

--- notify-slack/entrypoint.js
+++ notify-slack/entrypoint.js
@@ -8,11 +8,11 @@ const { context = {} } = github;
 const { pull_request } = context.payload;
 
 const main = async () => {
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
-  });
+  // await octokit.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: pull_request.number,
+  //   body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  // });
 };
 
 main();
