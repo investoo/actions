0166856531f9b6d6da05b40a554949b71abfc17c
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 12:15:51 +0000

    add test

---
+++ notify-slack-2/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -l
+
+export GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY
+export GITHUB_WORKFLOW_URL=$GITHUB_REPO_URL/actions/runs/$GITHUB_RUN_ID
+export GITHUB_COMMIT_URL=$GITHUB_REPO_URL/commit/$GITHUB_SHA
+
+if [ $DEPLOYMENT_SUCCESSFUL = true ]; then
+  export DEPLOYMENT_STATUS=Succeeded
+  export DEPLOYMENT_MESSAGE=":white_check_mark: *Deployment $DEPLOYMENT_STATUS*"
+else
+  export DEPLOYMENT_STATUS=Failed
+  export DEPLOYMENT_MESSAGE=":x: *$DEPLOYMENT_TARGET deployment $DEPLOYMENT_STATUS*"
+fi
+
+envsubst < /template.json > message.json
+
+# Debugging curl
+curl -v POST -H 'Content-type: application/json' --data '@message.json' https://offers.igms.io/api/v1/public/slack/send-slack
