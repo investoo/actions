e4045c757c28bdb9783c582b7b2c33bd9920e3ed
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 07:24:50 +0000

    comment

--- notify-slack/dist/index.js
+++ notify-slack/dist/index.js
@@ -30796,15 +30796,15 @@ const github = __nccwpck_require__(5438);
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
-const { context = {} } = github;
-const { pull_request } = context.payload;
+// const { context = {} } = github;
+// const { pull_request } = context.payload;
 
 const main = async () => {
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
-  });
+  // await octokit.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: pull_request.number,
+  //   body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  // });
 };
 
 main();
