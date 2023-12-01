bbb0b20d04b78a72de1f4d7e902e8aed20a40c3e
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-12-01 07:24:22 +0000

    comment

--- notify-slack/entrypoint.js
+++ notify-slack/entrypoint.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+// const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 // const octokit = github.getOctokit(GITHUB_TOKEN);
 
 // const { context = {} } = github;
